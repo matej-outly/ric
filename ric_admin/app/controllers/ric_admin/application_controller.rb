f9081e3e7021b40e5d256ddfbc6c8ea972acc722
Matěj Outlý <matej.outly@clockstar.cz>
2015-08-03 09:30:27 +0100

    Small fixes

---
+++ ric_admin/app/controllers/ric_admin/application_controller.rb
@@ -0,0 +1,15 @@
+# *****************************************************************************
+# * Copyright (c) Clockstar s.r.o. All rights reserved.
+# *****************************************************************************
+# *
+# * Abstract engine controller
+# *
+# * Author: Matěj Outlý
+# * Date  : 5. 5. 2015
+# *
+# *****************************************************************************
+
+module RicAdmin
+	class ApplicationController < ::ApplicationController
+	end
+end
