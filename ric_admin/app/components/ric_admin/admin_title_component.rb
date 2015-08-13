d35649e3658c32570a67aa9ed5b528e91282eff6
Matěj Outlý <matej.outly@clockstar.cz>
2015-08-13 14:45:25 +0100

    RIC Admin - configurable components

---
+++ ric_admin/app/components/ric_admin/admin_title_component.rb
@@ -0,0 +1,17 @@
+# *****************************************************************************
+# * Copyright (c) Clockstar s.r.o. All rights reserved.
+# *****************************************************************************
+# *
+# * Admin title
+# *
+# * Author: Matěj Outlý
+# * Date  : 13. 8. 2015
+# *
+# *****************************************************************************
+
+class RicAdmin::AdminTitleComponent < RugController::Component
+
+	def control
+	end
+
+end
--- ric_admin/app/controllers/ric_admin/application_controller.rb
+++ ric_admin/app/controllers/ric_admin/application_controller.rb
@@ -11,5 +11,41 @@
 
 module RicAdmin
 	class ApplicationController < ::ApplicationController
+
+		#
+		# Authenticate before every action
+		#
+		before_action :authenticate_user!
+
+		#
+		# Layout
+		#
+		layout "ric_admin"
+
+		#
+		# Title component
+		#
+		component RicAdmin::AdminTitleComponent
+
+		#
+		# Header logo component
+		#
+		component RicAdmin::AdminHeaderLogoComponent
+
+		#
+		# Header menu component
+		#
+		component RicAdmin::AdminHeaderMenuComponent
+		
+		#
+		# Footer menu component
+		#
+		component RicAdmin::AdminFooterMenuComponent
+
+		#
+		# Footer copy component
+		#
+		component RicAdmin::AdminFooterCopyComponent
+
 	end
 end
