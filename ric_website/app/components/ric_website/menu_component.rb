adcecc0e1ee7dca699e01a25c1d0808aef25941b
Matěj Outlý <matej.outly@clockstar.cz>
2015-09-03 13:52:45 +0100

    Menu component - Bugfix

--- ric_website/app/components/ric_website/menu_component.rb
+++ ric_website/app/components/ric_website/menu_component.rb
@@ -20,7 +20,9 @@ class RicWebsite::MenuComponent < RugController::Component
 		menu = RicWebsite.menu_model.where(key: config(:key)).first
 
 		# Menu items
-		if page_model.hierarchically_ordered?
+		if menu.nil?
+			@items = []
+		elsif page_model.hierarchically_ordered?
 			@items = menu.pages.where("depth <= :maximal_level", maximal_level: config(:maximal_level)).order(lft: :asc)
 		elsif page_model.ordered?
 			@items = menu.pages.order(position: :asc)
