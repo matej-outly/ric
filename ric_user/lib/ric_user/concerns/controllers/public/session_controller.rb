7ab525e94c9ebc2a436592de17104bceb3fc34f7
Matěj Outlý <matej.outly@clockstar.cz>
2015-12-22 14:33:41 +0100

    RicUser - session controller returns to referrer address

--- ric_user/lib/ric_user/concerns/controllers/public/session_controller.rb
+++ ric_user/lib/ric_user/concerns/controllers/public/session_controller.rb
@@ -44,15 +44,19 @@ module RicUser
 					# Edit action
 					#
 					def edit
+						puts request.referrer.inspect
+						puts ric_user_public.edit_session_url.inspect
+						store_return_path(request.referrer)
 					end
 
 					#
 					# Update action
 					#
 					def update
+						store_return_path(request.referrer) if request.referrer != ric_user_public.edit_session_url
 						if @session.update(session_params)
 							respond_to do |format|
-								format.html { redirect_to session_updated_path, notice: I18n.t("activerecord.notices.models.#{RicUser.session_model.model_name.i18n_key}.update") }
+								format.html { redirect_to (stored_return_path || session_updated_path), notice: I18n.t("activerecord.notices.models.#{RicUser.session_model.model_name.i18n_key}.update") }
 								format.json { render json: @session.id }
 							end
 						else
@@ -79,18 +83,46 @@ module RicUser
 						@session = RicUser.session_model.find_or_create(RicUser.session_model.current_id(session))
 					end
 
-					#
-					# Get path which should be followed after session is succesfully updated
-					#
-					def session_updated_path
-						ric_user_public.session_path
-					end
-
 					# 
 					# Never trust parameters from the scary internet, only allow the white list through.
 					#
 					def session_params
-						params.require(:session)
+						params.require(:session) # To be defined...
+					end
+
+					#
+					# Controller session key
+					#
+					def session_key
+						return "session"
+					end
+
+					#
+					# Store path to session
+					#
+					def store_return_path(return_path)
+						if session[session_key].nil?
+							session[session_key] = {}
+						end
+						session[session_key]["return_path"] = return_path
+					end
+
+					#
+					# Load path from session
+					#
+					def stored_return_path
+						if !session[session_key].nil? && !session[session_key]["return_path"].nil?
+							return session[session_key]["return_path"]
+						else
+							return nil
+						end
+					end
+
+					#
+					# Get path which should be followed after session is succesfully updated and no referrer found
+					#
+					def session_updated_path
+						ric_user_public.session_path
 					end
 
 				end
