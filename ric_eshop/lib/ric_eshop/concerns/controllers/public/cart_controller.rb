abd8f3a828bc7d0c93ba6cba1878542904bed12b
Matěj Outlý <matej.outly@clockstar.cz>
2016-03-15 08:53:02 +0100

    Bugfix

--- ric_eshop/lib/ric_eshop/concerns/controllers/public/cart_controller.rb
+++ ric_eshop/lib/ric_eshop/concerns/controllers/public/cart_controller.rb
@@ -39,7 +39,7 @@ module RicEshop
 					def add
 						my_params = cart_item_params
 						my_params = process_sub_product_ids(my_params)
-						my_params = process_product_panels(my_params)
+						my_params = process_product_variants(my_params)
 						
 						@cart.add(my_params)
 						result = @cart.save
@@ -74,7 +74,7 @@ module RicEshop
 					def remove
 						my_params = cart_item_params
 						my_params = process_sub_product_ids(my_params)
-						my_params = process_product_panels(my_params)
+						my_params = process_product_variants(my_params)
 
 						@cart.remove(my_params)
 						result = @cart.save
@@ -138,7 +138,7 @@ module RicEshop
 					# Never trust parameters from the scary internet, only allow the white list through.
 					#
 					def cart_item_params
-						params.permit([:product_id, :sub_product_ids].concat(params.keys.select { |key| key.to_s.start_with?("product_panel_") }))
+						params.permit([:product_id, :sub_product_ids].concat(params.keys.select { |key| key.to_s.start_with?("product_variant_") }))
 					end
 
 					def process_sub_product_ids(params)
@@ -148,14 +148,14 @@ module RicEshop
 						return params
 					end
 
-					def process_product_panels(params)
+					def process_product_variants(params)
 						if params[:sub_product_ids].is_a?(Array)
 							sub_product_ids = params[:sub_product_ids]
 						else
 							sub_product_ids = []
 						end
 						params.each do |key, value|
-							if key.to_s.start_with?("product_panel_")
+							if key.to_s.start_with?("product_variant_")
 								sub_product_ids << value
 								params.delete(key)
 							end
