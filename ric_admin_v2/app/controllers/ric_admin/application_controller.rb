613518feb6e729f3a1fe0fdb9bbb50189470ee48
Matěj Outlý <matej.outly@clockstar.cz>
2016-09-11 16:23:43 +0200

    RicAdmin - all components supplemented with simple partial views and it's configuration moved to module configuration

--- ric_admin_v2/app/controllers/ric_admin/application_controller.rb
+++ ric_admin_v2/app/controllers/ric_admin/application_controller.rb
@@ -22,30 +22,5 @@ module RicAdmin
 		#
 		layout "ric_admin"
 
-		#
-		# Title component
-		#
-		component RicAdmin::TitleComponent
-
-		#
-		# Header logo component
-		#
-		component RicAdmin::HeaderLogoComponent
-
-		#
-		# Header menu component
-		#
-		component RicAdmin::HeaderMenuComponent
-		
-		#
-		# Footer menu component
-		#
-		component RicAdmin::FooterMenuComponent
-
-		#
-		# Footer copy component
-		#
-		component RicAdmin::FooterCopyComponent
-
 	end
 end
