2ec28a62b79b7da99733e80dc05761d40e8e89b8
Matěj Outlý <matej.outly@clockstar.cz>
2016-11-04 10:55:04 +0100

    RicUrl - model concerns implementing slug generate methods (for hierarchical and for linear structures) - not tested

---
+++ ric_url/lib/ric_url/concerns/models/hierarchical_slug_generator.rb
@@ -0,0 +1,114 @@
+# *****************************************************************************
+# * Copyright (c) Clockstar s.r.o. All rights reserved.
+# *****************************************************************************
+# *
+# * Hierarchical slug generator
+# *
+# * Author: Matěj Outlý
+# * Date  : 21. 7. 2015
+# *
+# *****************************************************************************
+
+module RicUrl
+	module Concerns
+		module Models
+			module HierarchicalSlugGenerator extend ActiveSupport::Concern
+
+				#
+				# 'included do' causes the included code to be evaluated in the
+				# context where it is included, rather than being executed in 
+				# the module's context.
+				#
+				included do
+
+					after_save :generate_slugs
+
+					before_destroy :destroy_slugs_before, prepend: true
+
+					after_destroy :destroy_slugs_after
+
+				end
+
+				# *************************************************************
+				# Hooks
+				# *************************************************************
+
+				def generate_slugs(options = {})
+					
+					# Generate slug in this model
+					if !RicUrl.slug_model.nil?
+						I18n.available_locales.each do |locale|
+							self._destroy_slug_was(RicUrl.slug_model, locale)
+							self._generate_slug(RicUrl.slug_model, locale)
+						end
+					end
+
+					# Propagate to other models
+					if options[:disable_propagation] != true
+						
+						# Propagate to parent
+						self.parent.generate_slugs(disable_propagation: true) if self.parent
+
+						# Propagate to descendants
+						self.descendants.each do |descendant|
+							descendant.generate_slugs(disable_propagation: true)
+						end
+
+					end
+
+				end
+
+				def destroy_slugs_before(options = {})
+
+					# Destroy slug of this model
+					if !RicUrl.slug_model.nil?
+						self._destroy_slug(RicUrl.slug_model, locale)
+					end
+
+					# Propagate to other models
+					if options[:disable_propagation] != true
+						
+						# Propagate to descendants
+						self.descendants.each do |descendant|
+							descendant.destroy_slugs_before(disable_propagation: true)
+						end
+
+					end
+
+				end
+
+				def destroy_slugs_after(options = {})
+
+					# Propagate to other models
+					if options[:disable_propagation] != true
+						
+						# Propagate to parent
+						self.parent.generate_slugs(disable_propagation: true) if self.parent
+
+					end
+
+				end
+
+			protected
+
+				# *************************************************************
+				# Callbacks to be defined in apllication
+				# *************************************************************
+
+				def _generate_slug(slug_model, locale)
+					raise "No be defined in application."
+				end
+
+				def _destroy_slug(slug_model, locale)
+					raise "No be defined in application."
+				end
+
+				def _destroy_slug_was(slug_model, locale)
+					raise "No be defined in application."
+				end
+
+
+			end
+		end
+	end
+end
---
+++ ric_url/lib/ric_url/concerns/models/slug_generator.rb
@@ -0,0 +1,72 @@
+# *****************************************************************************
+# * Copyright (c) Clockstar s.r.o. All rights reserved.
+# *****************************************************************************
+# *
+# * Linear slug generator
+# *
+# * Author: Matěj Outlý
+# * Date  : 21. 7. 2015
+# *
+# *****************************************************************************
+
+module RicUrl
+	module Concerns
+		module Models
+			module SlugGenerator extend ActiveSupport::Concern
+
+				#
+				# 'included do' causes the included code to be evaluated in the
+				# context where it is included, rather than being executed in 
+				# the module's context.
+				#
+				included do
+
+					after_save :generate_slugs
+
+					before_destroy :destroy_slugs, prepend: true
+
+				end
+
+				# *************************************************************
+				# Hooks
+				# *************************************************************
+
+				def generate_slugs(options = {})
+					
+					# Generate slug in this model
+					if !RicUrl.slug_model.nil?
+						I18n.available_locales.each do |locale|
+							self._destroy_slug_was(RicUrl.slug_model, locale)
+							self._generate_slug(RicUrl.slug_model, locale)
+						end
+					end
+
+				end
+
+				def destroy_slugs(options = {})
+
+					# Destroy slug of this model
+					if !RicUrl.slug_model.nil?
+						self._destroy_slug(RicUrl.slug_model, locale)
+					end
+
+				end
+
+			protected
+
+				# *************************************************************
+				# Callbacks to be defined in apllication
+				# *************************************************************
+
+				def _generate_slug(slug_model, locale)
+					raise "No be defined in application."
+				end
+
+				def _destroy_slug(slug_model, locale)
+					raise "No be defined in application."
+				end
+
+			end
+		end
+	end
+end
