c42b74bd86f41c2899c56ab89cc9e4beaf617941
Jaroslav Mlejnek <jar.gml55@gmail.com>
2017-02-23 14:55:32 +0100

    Authorization concern move out from controller folder.

--- ric_dms/lib/ric_dms/concerns/authorization.rb
+++ ric_dms/lib/ric_dms/concerns/authorization.rb
@@ -11,26 +11,24 @@
 
 module RicDms
 	module Concerns
-		module Controllers
-			module Authorization extend ActiveSupport::Concern
+		module Authorization extend ActiveSupport::Concern
 
-				included do
-					helper_method :can_read?
-					helper_method :can_read_and_write?
-				end
-
-				def can_read?
-					true
-				end
-
-				def can_read_and_write?
-					true
-				end
-
-				def not_autorized!
-				end
-
+			included do
+				helper_method :can_read?
+				helper_method :can_read_and_write?
 			end
+
+			def can_read?
+				true
+			end
+
+			def can_read_and_write?
+				true
+			end
+
+			def not_autorized!
+			end
+
 		end
 	end
 end
--- ric_dms/lib/ric_dms/engine.rb
+++ ric_dms/lib/ric_dms/engine.rb
@@ -15,10 +15,14 @@ module RicDms
 		#
 		# Controllers
 		#
-		require 'ric_dms/concerns/controllers/authorization'
 		require 'ric_dms/concerns/controllers/documents_controller'
 		require 'ric_dms/concerns/controllers/document_versions_controller'
 		require 'ric_dms/concerns/controllers/document_folders_controller'
+
+		#
+		# Authorization
+		#
+		require 'ric_dms/concerns/authorization'
 
 		isolate_namespace RicDms
 
