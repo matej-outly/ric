6cb5ac46fc11b8451dee22f474d076a83059157c
Matěj Outlý <matej.outly@clockstar.cz>
2017-03-24 19:12:27 +0100

    RicAuth - small fix in override role routine

--- ric_auth/lib/ric_auth/concerns/overrides.rb
+++ ric_auth/lib/ric_auth/concerns/overrides.rb
@@ -20,11 +20,16 @@ module RicAuth
 			def override_role_if_possible
 				if current_user
 					if session["auth_overrides"] && session["auth_overrides"]["role"]
-						current_user.current_role = session["auth_overrides"]["role"]
-						role_changed = session["auth_overrides"]["role_changed"]
-						session["auth_overrides"]["role_changed"] = false
-						if role_changed
-							redirect_to current_root_path
+						if current_user.roles.include?(session["auth_overrides"]["role"])
+							current_user.current_role = session["auth_overrides"]["role"]
+							role_changed = session["auth_overrides"]["role_changed"]
+							session["auth_overrides"]["role_changed"] = false
+							if role_changed
+								redirect_to current_root_path
+							end
+						else
+							session["auth_overrides"].delete("role")
+							session["auth_overrides"]["role_changed"] = false
 						end
 					end
 				end
