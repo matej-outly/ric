b9bfeb03bfbfe6956222455cd51104bcfedc2528
Matěj Outlý <matej.outly@clockstar.cz>
2017-04-08 11:00:07 +0200

    RicContact - contact message notifications

--- ric_contact/lib/ric_contact/concerns/mailers/contact_message_mailer.rb
+++ ric_contact/lib/ric_contact/concerns/mailers/contact_message_mailer.rb
@@ -16,27 +16,46 @@ module RicContact
 
 				def new_message(contact_message)
 					@contact_message = contact_message
-					
-					# Params
 					params = {}
+					
+					# Subject
 					params[:subject] = I18n.t("activerecord.mailers.ric_contact.contact_message.new_message.subject")
 					
 					# Sender
 					params[:from] = RicContact.mailer_sender
-					if params[:from].nil?
-						raise "Please specify sender."
+					raise "Please specify sender." if params[:from].nil?
+					if !RicContact.mailer_sender_name.blank?
+						params[:from] = "#{RicContact.mailer_sender_name} <#{params[:from]}>"
 					end
 
 					# Receiver
 					params[:to] = RicContact.mailer_receiver
-					if params[:to].nil?
-						raise "Please specify receiver."
-					end
+					raise "Please specify receiver." if params[:to].nil?
 
 					# Copy for author
-					if RicContact.send_contact_messages_copy_to_author && contact_message.respond_to?(:email) && !contact_message.email.blank?
-						params[:bcc] = contact_message.email
+					params[:bcc] = contact_message.email if RicContact.send_contact_messages_copy_to_author && contact_message.respond_to?(:email) && !contact_message.email.blank?
+					
+					mail(params)
+				end
+
+				def notify_message(contact_message)
+					@contact_message = contact_message
+					params = {}
+					
+					# Subject
+					params[:subject] = I18n.t("activerecord.mailers.ric_contact.contact_message.notify_message.subject")
+					
+					# Sender
+					params[:from] = RicContact.mailer_sender
+					raise "Please specify sender." if params[:from].nil?
+					if !RicContact.mailer_sender_name.blank?
+						params[:from] = "#{RicContact.mailer_sender_name} <#{params[:from]}>"
 					end
+					
+					# Receiver
+					raise "Contect message model must respond to email attribute" if !contact_message.respond_to?(:email) 
+					return if contact_message.email.blank?
+					params[:to] = contact_message.email 
 
 					mail(params)
 				end
