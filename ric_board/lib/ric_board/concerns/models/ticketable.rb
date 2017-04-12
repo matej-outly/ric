dd9e5269c57bb3447f1c15f2c8e13947d4df6b29
Jaroslav Mlejnek <jar.gml55@gmail.com>
2017-04-12 16:06:23 +0200

    Ric board now supports multiple owners

--- ric_board/lib/ric_board/concerns/models/ticketable.rb
+++ ric_board/lib/ric_board/concerns/models/ticketable.rb
@@ -33,6 +33,7 @@ module RicBoard
 					# Callbacks
 					# *********************************************************
 
+					after_initialize :initialize_owner_was
 					after_find :save_owner_after_find
 					after_create :create_board_ticket_after_create
 					after_update :create_board_ticket_after_update
@@ -57,8 +58,22 @@ module RicBoard
 
 			protected
 
+				def initialize_owner_was
+					if @owner_was.nil?
+						@owner_was = []
+					end
+				end
+
 				def save_owner_after_find
+					# Get current owner
 					@owner_was = board_ticket_params[:owner] if !board_ticket_params.blank?
+
+					# Convert owner into array
+					if @owner_was.nil?
+						@owner_was = []
+					elsif !@owner_was.is_a?(Array)
+						@owner_was = [@owner_was]
+					end
 				end
 
 				def create_board_ticket_after_create
@@ -81,9 +96,18 @@ module RicBoard
 					if !params.include?(:owner)
 						raise "Board ticket `owner` must be set in board ticket params"
 					end
+
+					# Convert owner into array
+					owner = params[:owner]
+					if owner.nil?
+						owner = []
+					elsif !owner.is_a?(Array)
+						owner = [owner]
+					end
+
 					return {
 						date: (params.include?(:date) ? params[:date] : nil),
-						owner: params[:owner],
+						owner: owner,
 						key: (params[:key].blank? ? self.class.to_s.gsub("::", "_").underscore.pluralize : params[:key]), # Either defived from class name or defined key
 					}
 				end
@@ -97,19 +121,19 @@ module RicBoard
 
 					# Check if owner of the ticket is changed
 					# If so, destroy old ticket
-					if @owner_was && params[:owner] != @owner_was
-						old_board_ticket = RicBoard.board_ticket_model.find_by(subject: self, owner: @owner_was)
-						old_board_ticket.destroy unless old_board_ticket.nil?
-					end
-
-					# Create or update board ticket
-					unless params[:owner].nil?
-						board_ticket = RicBoard.board_ticket_model.find_or_initialize_by(subject: self, owner: params[:owner])
-						board_ticket.date = params[:date]
-						board_ticket.key = params[:key]
-						board_ticket.occasion = occasion
-						board_ticket.closed = false
-						board_ticket.save
+					@owner_was.diff(params[:owner]) do |action, owner|
+						if action == :remove
+							old_board_ticket = RicBoard.board_ticket_model.find_by(subject: self, owner: owner)
+							old_board_ticket.destroy unless old_board_ticket.nil?
+						else # add or equal
+							# Create or update board ticket
+							board_ticket = RicBoard.board_ticket_model.find_or_initialize_by(subject: self, owner: owner)
+							board_ticket.date = params[:date]
+							board_ticket.key = params[:key]
+							board_ticket.occasion = occasion
+							board_ticket.closed = false
+							board_ticket.save
+						end
 					end
 				end
 
