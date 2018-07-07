3f2b17e87f4a41c2671b58eb86c69fbfd8a07edc
Matěj Outlý <matej.outly@clockstar.cz>
2018-07-07 22:58:15 +0200

    RicDms - Icons and UI fixes

--- ric_dms/lib/ric_dms/helpers/mime_type_helper.rb
+++ ric_dms/lib/ric_dms/helpers/mime_type_helper.rb
@@ -16,27 +16,27 @@ module RicDms
 			def mime_type_fa_icon(content_type)
 				icon_classes = {
 					# Media
-					'image' => 'fa-file-image-o',
-					'audio' => 'fa-file-audio-o',
-					'video' => 'fa-file-video-o',
+					'image' => 'fa-file-image',
+					'audio' => 'fa-file-audio',
+					'video' => 'fa-file-video',
 					# Documents
-					'application/pdf' => 'fa-file-pdf-o',
-					'application/msword' => 'fa-file-word-o',
-					'application/vnd.ms-word' => 'fa-file-word-o',
-					'application/vnd.oasis.opendocument.text' => 'fa-file-word-o',
-					'application/vnd.openxmlformats-officedocument.wordprocessingml' => 'fa-file-word-o',
-					'application/vnd.ms-excel' => 'fa-file-excel-o',
-					'application/vnd.openxmlformats-officedocument.spreadsheetml' => 'fa-file-excel-o',
-					'application/vnd.oasis.opendocument.spreadsheet' => 'fa-file-excel-o',
-					'application/vnd.ms-powerpoint' => 'fa-file-powerpoint-o',
-					'application/vnd.openxmlformats-officedocument.presentationml' => 'fa-file-powerpoint-o',
-					'application/vnd.oasis.opendocument.presentation' => 'fa-file-powerpoint-o',
-					'text/plain' => 'fa-file-text-o',
-					'text/html' => 'fa-file-code-o',
-					'application/json' => 'fa-file-code-o',
+					'application/pdf' => 'fa-file-pdf',
+					'application/msword' => 'fa-file-word',
+					'application/vnd.ms-word' => 'fa-file-word',
+					'application/vnd.oasis.opendocument.text' => 'fa-file-word',
+					'application/vnd.openxmlformats-officedocument.wordprocessingml' => 'fa-file-word',
+					'application/vnd.ms-excel' => 'fa-file-excel',
+					'application/vnd.openxmlformats-officedocument.spreadsheetml' => 'fa-file-excel',
+					'application/vnd.oasis.opendocument.spreadsheet' => 'fa-file-excel',
+					'application/vnd.ms-powerpoint' => 'fa-file-powerpoint',
+					'application/vnd.openxmlformats-officedocument.presentationml' => 'fa-file-powerpoint',
+					'application/vnd.oasis.opendocument.presentation' => 'fa-file-powerpoint',
+					'text/plain' => 'fa-file-text',
+					'text/html' => 'fa-file-code',
+					'application/json' => 'fa-file-code',
 					# Archives
-					'application/gzip' => 'fa-file-archive-o',
-					'application/zip' => 'fa-file-archive-o',
+					'application/gzip' => 'fa-file-archive',
+					'application/zip' => 'fa-file-archive',
 				}
 
 				icon_classes.each do |mime_type, icon_class|
@@ -45,7 +45,7 @@ module RicDms
 					end
 				end
 
-				return "fa-file-o"
+				return "fa-file"
 			end
 
 		end
