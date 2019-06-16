da5832c48ee3aa6eee35fb8a9b2a24ac7b2deecd
Matěj Outlý <matej.outly@clockstar.cz>
2019-06-16 10:43:00 +0200

    RicAuth - password enforcement disabled for overriden users

--- ric_auth/lib/ric_auth/concerns/password_enforcement.rb
+++ ric_auth/lib/ric_auth/concerns/password_enforcement.rb
@@ -24,10 +24,14 @@ module RicAuth
 			end
 
 			def check_password_enforcement
-				if @disable_password_enforcement != true && !current_user.nil?
-					if current_user.password_changed_at.nil? || (!current_user.password_forced_at.nil? && current_user.password_changed_at < current_user.password_forced_at)
-						store_location_for(:user, request.path)
-						redirect_to ric_auth.edit_profile_password_path, alert: RicAuth.user_model.human_error_message(:password_enforced)
+				if @disable_password_enforcement != true # Overall disable
+					if !current_user.nil? # User is logged in
+						if defined?(:real_current_user) && real_current_user && real_current_user.id == current_user.id # Logged user must be real user (-> disable enforcement if user overrriden)
+							if current_user.password_changed_at.nil? || (!current_user.password_forced_at.nil? && current_user.password_changed_at < current_user.password_forced_at)
+								store_location_for(:user, request.path)
+								redirect_to ric_auth.edit_profile_password_path, alert: RicAuth.user_model.human_error_message(:password_enforced)
+							end
+						end
 					end
 				end
 			end
